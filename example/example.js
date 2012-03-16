81b5632618b488cdb6cc104bc95cba20a74192a2
isaacs <i@izs.me>
2012-03-16 16:16:11 -0700

    bsd lic

--- example/example.js
+++ example/example.js
@@ -3,10 +3,12 @@ var read = require("../lib/read.js")
 read({prompt: "Username: ", default: "test-user" }, function (er, user) {
   read({prompt: "Password: ", default: "test-pass", silent: true }, function (er, pass) {
     read({prompt: "Enter 4 characters: ", num: 4 }, function (er, four) {
-      console.error(user + ":" + pass + " " + JSON.stringify(four))
-      console.error("If the program doesn't end right now,\n"
-                   +"then you may be experiencing this bug:\n"
-                   +"https://github.com/joyent/node/issues/2257")
+      read({prompt: "Password again: ", default: "test-pass", silent: true }, function (er, pass2) {
+        console.error(user + ":" + pass + " (" + pass2 + ") " + JSON.stringify(four) + (pass === pass2))
+        console.error("If the program doesn't end right now,\n"
+                     +"then you may be experiencing this bug:\n"
+                     +"https://github.com/joyent/node/issues/2257")
+      })
     })
   })
 })
