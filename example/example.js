6f8101c11ecf9f1bc77e0efe6a5500cb5e1aa495
isaacs <i@izs.me>
2012-06-13 18:11:11 -0700

    Make example more clear

--- example/example.js
+++ example/example.js
@@ -4,7 +4,11 @@ read({prompt: "Username: ", default: "test-user" }, function (er, user) {
   read({prompt: "Password: ", default: "test-pass", silent: true }, function (er, pass) {
     read({prompt: "Enter 4 characters: ", num: 4 }, function (er, four) {
       read({prompt: "Password again: ", default: "test-pass", silent: true }, function (er, pass2) {
-        console.error(user + ":" + pass + " (" + pass2 + ") " + JSON.stringify(four) + (pass === pass2))
+        console.error({user: user,
+                       pass: pass,
+                       verify: pass2,
+                       four:four,
+                       passMatch: (pass === pass2)})
         console.error("If the program doesn't end right now,\n"
                      +"then you may be experiencing this bug:\n"
                      +"https://github.com/joyent/node/issues/2257")
