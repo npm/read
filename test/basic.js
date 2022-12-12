c5b56f6242493173ded23a97f4fd2ffb4017310f
Luke Karrys <luke@lukekarrys.com>
2022-12-11 18:13:59 -0700

    feat: use async/await
    BREAKING CHANGE: this module has been refactored to use promises
    - the API is now promise only and no longer accepts a callback
    - the Promise is resolved to a string and no longer returns `isDefault`

--- test/basic.js
+++ test/basic.js
@@ -1,61 +1,54 @@
-var read = require('../lib/read.js')
+const t = require('tap')
+const read = require('../')
+const spawnRead = require('./fixtures/setup')
 
 if (process.argv[2] === 'child') {
   return child()
 }
 
-var tap = require('tap')
-var CLOSE = 'close'
-if (process.version.match(/^v0\.6/)) {
-  CLOSE = 'exit'
+async function child () {
+  const user = await read({ prompt: 'Username: ', default: 'test-user' })
+  const pass = await read({ prompt: 'Password: ', default: 'test-pass', silent: true })
+  const verify = await read({ prompt: 'Password again: ', default: 'test-pass', silent: true })
+
+  console.error(JSON.stringify({
+    user,
+    pass,
+    verify,
+    passMatch: pass === verify,
+  }))
+
+  if (process.stdin.unref) {
+    process.stdin.unref()
+  }
 }
 
-var spawn = require('child_process').spawn
-
-tap.test('basic', function (t) {
-  var child = spawn(process.execPath, [__filename, 'child'])
-  var output = ''
-  var write = child.stdin.write.bind(child.stdin)
-  child.stdout.on('data', function (c) {
-    console.error('data %s', c)
-    output += c
-    if (output.match(/Username: \(test-user\) $/)) {
-      process.nextTick(write.bind(null, 'a user\n'))
-    } else if (output.match(/Password: \(<default hidden>\) $/)) {
-      process.nextTick(write.bind(null, 'a password\n'))
-    } else if (output.match(/Password again: \(<default hidden>\) $/)) {
-      process.nextTick(write.bind(null, 'a password\n'))
-    } else {
-      console.error('prompts done, output=%j', output)
-    }
+t.test('basic', async (t) => {
+  const { stdout, stderr } = await spawnRead(__filename, {
+    'Username: (test-user)': 'a user',
+    'Password: (<default hidden>)': 'a password',
+    'Password again: (<default hidden>)': 'a password',
   })
 
-  var result = ''
-  child.stderr.on('data', function (c) {
-    result += c
-    console.error('result %j', c.toString())
-  })
-
-  child.on(CLOSE, function () {
-    result = JSON.parse(result)
-    t.same(result, { user: 'a user', pass: 'a password', verify: 'a password', passMatch: true })
-    t.equal(output, 'Username: (test-user) Password: (<default hidden>) Password again: (<default hidden>) ')
-    t.end()
-  })
+  t.same(JSON.parse(stderr),
+    { user: 'a user', pass: 'a password', verify: 'a password', passMatch: true })
+  t.equal(stdout,
+    'Username: (test-user) Password: (<default hidden>) Password again: (<default hidden>) ')
 })
 
-function child () {
-  read({ prompt: 'Username: ', default: 'test-user' }, function (er, user) {
-    read({ prompt: 'Password: ', default: 'test-pass', silent: true }, function (er, pass) {
-      read({ prompt: 'Password again: ', default: 'test-pass', silent: true }, function (er, pass2) {
-        console.error(JSON.stringify({ user: user,
-          pass: pass,
-          verify: pass2,
-          passMatch: (pass === pass2) }))
-        if (process.stdin.unref) {
-          process.stdin.unref()
-        }
-      })
-    })
+t.test('defaults', async (t) => {
+  const { stdout, stderr } = await spawnRead(__filename, {
+    'Username: (test-user)': '',
+    'Password: (<default hidden>)': '',
+    'Password again: (<default hidden>)': '',
   })
-}
+
+  t.same(JSON.parse(stderr),
+    { user: 'test-user', pass: 'test-pass', verify: 'test-pass', passMatch: true })
+  t.equal(stdout,
+    'Username: (test-user) Password: (<default hidden>) Password again: (<default hidden>) ')
+})
+
+t.test('errors', async (t) => {
+  t.rejects(() => read({ default: {} }))
+})
